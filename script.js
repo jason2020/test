73013ea2082d55b66e927b03f24ba6e802a46335
jason2020 <jtay20@punahou.edu>
2024-02-18 23:38:13 -1000

    Added UI

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const submitButton = document.getElementById('login');
+const submitButton = document.getElementById('loginButton');
 submitButton.addEventListener('click', () => {
     alert("What are you doing silly, don't press sketchy buttons!")
 });
